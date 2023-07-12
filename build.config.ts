58fa2c068abf3e5db51ed5fb7226af3f94aca030
halo <1654403734@qq.com>
2023-07-12 16:08:27 +0800

    optim: 将 jsonSchema模板文件生成挪到build过程

--- build.config.ts
+++ build.config.ts
@@ -1,7 +1,6 @@
 import { defineBuildConfig } from 'unbuild'
-import ts2 from 'rollup-plugin-typescript2'
 import { banner } from './scripts/rollup-banner-plugin'
-import { dtsMapping } from './scripts/rollup-dts-mapping-plugin'
+import { generateJsonSchema } from './scripts/generate-json-schema-script'
 
 export default defineBuildConfig({
     entries: ['src/index'],
@@ -12,14 +11,14 @@ export default defineBuildConfig({
         inlineDependencies: true
     },
     hooks: {
+        // 构建开始前, 生成 json schema 文件
+        'build:prepare'(): void {
+            generateJsonSchema()
+        },
         'rollup:options'(_, options): void {
             options.plugins = [
                 // 插入banner
                 banner(),
-                // 使用自定义的 .d.ts 转化插件
-                ts2({ tsconfigDefaults: { emitDeclarationOnly: true } }),
-                //  映射 .d.ts 文件路径 (处理存在 src/ 目录外引用导致的.d.ts输出目录层级与预期不一致问题)
-                dtsMapping((name: string) => name.replace(/^src/, 'typings')),
                 // unbuild 构建插件
                 options.plugins
             ]
